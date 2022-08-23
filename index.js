7e77c57050c8579d47be3c03bad16adcc9086abf
Adam Nagy <adam_e_nagy@hotmail.com>
2022-08-23 23:05:29 +0100

    Handle custom credentials and refresh token

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from "url";
 import express from "express";
 import axios from "axios";
 import cookieSession from "cookie-session";
+import { url } from "inspector";
 
 let app = express();
 
@@ -10,7 +11,7 @@ app.use(
   cookieSession({
     name: "forge_session",
     keys: ["forge_secure_key"],
-    maxAge: 60 * 60 * 1000 // 1 hour like the token
+    maxAge: 60 * 60 * 1000 // 1 hour like the token (changes to cookie content resets the timer)
   })
 );
 
@@ -22,15 +23,13 @@ let callbackUrl =
   process.env.FORGE_CALLBACK_URL || `${serverUrl}/callback/oauth`;
 
 app.get("/callback/oauth", async (req, res) => {
+  console.log("/callback/oauth", req.session);
+
   const { code } = req.query;
 
   try {
-    let cId = clientId;
-    let cSecret = clientSecret;
-    if (req.session.client_id && req.session.client_secret) {
-      cId = req.session.client_id;
-      cSecret = req.session.client_secret;
-    }
+    let cId = req.session.client_id ? req.session.client_id : clientId;
+    let cSecret = req.session.client_secret ? req.session.client_secret : clientSecret;
 
     const response = await axios({
       method: "POST",
@@ -41,11 +40,15 @@ app.get("/callback/oauth", async (req, res) => {
       data: `client_id=${cId}&client_secret=${cSecret}&grant_type=authorization_code&code=${code}&redirect_uri=${callbackUrl}`
     });
 
-    req.session = {
-      access_token: response.data.access_token
-    };
+    req.session = req.session || {};
+    req.session.access_token = response.data.access_token;
+    req.session.refresh_token = response.data.refresh_token;
 
-    res.redirect("/");
+    if (req.session.client_id && req.session.client_secret) {
+      res.redirect(`/?client_id=${req.session.client_id}&client_secret=${req.session.client_secret}`);
+    } else {
+      res.redirect(`/`);
+    }
   } catch (error) {
     console.log(error);
     res.end();
@@ -53,26 +56,61 @@ app.get("/callback/oauth", async (req, res) => {
 });
 
 app.get("/oauth/token", async (req, res) => {
-  console.log(req.session);
+  console.log("/oauth/token", req.session);
+
+  if (req.query.client_id && req.query.client_secret) {
+    // If credentials changed
+    if (req.query.client_id !== req.session.client_id) {
+      req.session = {
+        client_id: req.query.client_id,
+        client_secret: req.query.client_secret
+      };
+      res.status(401).end();
+      return;
+    }
+  } else {
+    // If credentials changed
+    if (req.session.client_id) {
+      req.session = null;
+      res.status(401).end();
+      return;
+    }
+  }
+
   if (!req.session?.access_token) {
     res.status(401).end();
     return;
   }
 
-  res.end(req.session.access_token);
+  let access_token = req.session.access_token;
+
+  if (req.query.refresh) {
+    let cId = req.session.client_id ? req.session.client_id : clientId;
+    let cSecret = req.session.client_secret ? req.session.client_secret : clientSecret;
+    let rToken = req.session.refresh_token;
+    const response = await axios({
+      method: "POST",
+      url: "https://developer.api.autodesk.com/authentication/v1/refreshtoken",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: `client_id=${cId}&client_secret=${cSecret}&grant_type=refresh_token&refresh_token=${rToken}`
+    });
+
+    req.session = req.session || {};
+    req.session.access_token = response.data.access_token;
+    req.session.refresh_token = response.data.refresh_token;
+
+    access_token = response.data.access_token;
+  }
+
+  res.end(access_token);
 });
 
 app.get("/oauth/url", (req, res) => {
-  let cId = clientId;
-  let cSecret = clientSecret;
-  if (req.query.client_id && req.query.client_secret) {
-    cId = req.query.client_id;
-    cSecret = req.query.client_secret;
-    req.session = {
-      client_id: cId,
-      client_secret: cSecret
-    };
-  } 
+  console.log("/oauth/url", req.session);
+
+  let cId = req.session.client_id ? req.session.client_id : clientId;
 
   const url =
     "https://developer.api.autodesk.com" +
